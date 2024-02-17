52cddca8d8534d5b7659dd12b0f25af53719f8cf
Anamul029 <anamhaque.5101@gmail.com>
2024-02-17 13:17:35 +0600

    finishing main part of this game

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -28,32 +28,60 @@ function handlekeybordButtonPress(event) {
     //   get the expected to press
     const currentElement = document.getElementById('currentElement')
     const currentAlphabet = currentElement.innerText;
-    const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
-    console.log(playerPressed, expectedAlphabet)
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    // console.log(playerPressed, expectedAlphabet)
 
     // checked matching or not
 
-    if(playerPressed===expectedAlphabet){
-        console.log('you win');
+    if (playerPressed === expectedAlphabet) {
         // update score
-        const score=document.getElementById('current-score');
-        const totalScore=(score.innerText)++;
-        
+        const score = document.getElementById('current-score');
+        const totalScore = (score.innerText)++;
+
+
         // start a new round
-        removeBackgroundColorById(expectedAlphabet)
+        removeBackgroundColorById(expectedAlphabet);
         continueGame();
     }
-    else{
-        console.log('you miss and you loss a life')
-        const life=document.getElementById('current-life');
-        const totalLife=(life.innerText)--;
-        if(totalLife===0){
-            const currentSec=document.getElementById('play');
+    else {
+        
+        // console.log('you miss and you loss a life')
+        const life = document.getElementById('current-life');
+        const totalLife = (life.innerText)--;
+        const Totallife=parseInt(totalLife);
+        if (Totallife === 0) {
+             // clear the last selected alphabet background
+            
+            const currentSec = document.getElementById('play');
             currentSec.classList.add('hidden')
-           const endGame=document.getElementById('endPart');
-           endGame.classList.remove('hidden')
+            const endGame = document.getElementById('endPart');
+            endGame.classList.remove('hidden')
+            // showing final score by id
+            const finalScore = document.getElementById('final-score');
+            //  current score by id
+            const score = document.getElementById('current-score');
+            finalScore.innerText = score.innerText;
+            // play again
+            document.getElementById('play-again-btn').addEventListener('click', function () {
+                // set the initial life and score by id
+                score.innerText = '0';
+                life.innerText = '5';
+                // remove last alphabet backgground-color
+                // step1 get the alphabet by id
+                const lastAlpha=getElementTextById('currentElement');
+                // then remove backgroun bg
+                removeBackgroundColorById(lastAlpha);
+                hideElementById('endPart');
+                showElementById('play');
+                continueGame();
+               
+
+
+            })
         }
+
     }
 
 }
 document.addEventListener('keyup', handlekeybordButtonPress)
+
--- scripts/functions.js
+++ scripts/functions.js
@@ -21,3 +21,21 @@ function removeBackgroundColorById(elementId){
     const element=document.getElementById(elementId);
     element.classList.remove('bg-orange-400')
 }
+// hide element by id
+
+function hideElementById(id){
+    const element=document.getElementById(id);
+    element.classList.add('hidden');
+}
+// show element by id
+function showElementById(id){
+    const element=document.getElementById(id);
+    element.classList.remove('hidden');
+}
+
+// get element text by id
+function getElementTextById(id){
+    const element=document.getElementById(id);
+    const alpha=element.innerText;
+    return alpha;
+}
