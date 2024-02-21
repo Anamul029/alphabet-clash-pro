09282b8bdd74e42374adace74f20aed8fd265973
Anamul029 <anamhaque.5101@gmail.com>
2024-02-21 14:53:13 +0600

    customize sound system

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -4,6 +4,7 @@ function play() {
     home.classList.add('hidden')
     const play = document.getElementById('play');
     play.classList.remove('hidden')
+    isgamePlay = true;
     // step2 show the playground
     // playing game
     continueGame();
@@ -21,38 +22,46 @@ function continueGame() {
     addBackgroundColorById(alphabet)
 
 }
-
+// initially is gameplay is false
+isgamePlay = false;
 // captured keybord key press
 function handlekeybordButtonPress(event) {
     const playerPressed = event.key;
     //press the 'esc' button to stop the game
     if (playerPressed === 'Escape') {
-         //  go to the end section of this game
+        //  go to the end section of this game
         const currentSec = document.getElementById('play');
-            currentSec.classList.add('hidden')
-            const endGame = document.getElementById('endPart');
-            endGame.classList.remove('hidden')
-            // showing final score by id
-            const finalScore = document.getElementById('final-score');
-            //  current score by id
-            const score = document.getElementById('current-score');
-            finalScore.innerText = score.innerText;
-            // play again
-            document.getElementById('play-again-btn').addEventListener('click', function () {
-                // set the initial life and score by id
-                score.innerText = '0';
-                life.innerText = '5';
-                // remove last alphabet backgground-color
-                // step1 get the alphabet by id
-                const lastAlpha = getElementTextById('currentElement');
-                // then remove backgroun bg
-                removeBackgroundColorById(lastAlpha);
-                hideElementById('endPart');
-                showElementById('play');
-                continueGame();
+        currentSec.classList.add('hidden')
+        const endGame = document.getElementById('endPart');
+        endGame.classList.remove('hidden')
+        // showing final score by id
+        const finalScore = document.getElementById('final-score');
+        //  current score by id
+        const score = document.getElementById('current-score');
+        finalScore.innerText = score.innerText;
+        // play again
+        document.getElementById('play-again-btn').addEventListener('click', function () {
+            // set the initial life and score by id
+            score.innerText = '0';
+            life.innerText = '5';
+            // remove last alphabet backgground-color
+            // step1 get the alphabet by id
+            const lastAlpha = getElementTextById('currentElement');
+            // then remove backgroun bg
+            removeBackgroundColorById(lastAlpha);
+            hideElementById('endPart');
+            showElementById('play');
+            continueGame();
 
-            })
+        })
     }
+
+//    if game is not started that time sound is not provided
+    if(isgamePlay===false){
+        return;
+    }
+
+
     //   get the expected to press
     const currentElement = document.getElementById('currentElement')
     const currentAlphabet = currentElement.innerText;
@@ -65,8 +74,8 @@ function handlekeybordButtonPress(event) {
         // update score
         const score = document.getElementById('current-score');
         const totalScore = (score.innerText)++;
-    //    play audio
-          playAudio('right');
+        //    play audio
+        playAudio('right');
 
         // start a new round
         removeBackgroundColorById(expectedAlphabet);
@@ -92,9 +101,13 @@ function handlekeybordButtonPress(event) {
             //  current score by id
             const score = document.getElementById('current-score');
             finalScore.innerText = score.innerText;
+            // isgameplay is false
+            isgamePlay=false;
             // play again
             document.getElementById('play-again-btn').addEventListener('click', function () {
                 // set the initial life and score by id
+                // isgameplay is true
+                isgamePlay=true;
                 score.innerText = '0';
                 life.innerText = '5';
                 // remove last alphabet backgground-color
